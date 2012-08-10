ffe12071fd555f0d1cd77b600bec01054355d479
Sean Wilkinson <sean@mathbiol.org>
2012-08-10 15:31:47 -0500

    Replaced `Blob` stub with `Reference` and `Resource` stubs

--- constructors.js
+++ constructors.js
@@ -2,6 +2,7 @@
 
 //- constructors.js ~~
 //                                                      ~~ (c) SRW, 01 Aug 2012
+//                                                  ~~ last updated 10 Aug 2012
 
 (function () {
     'use strict';
@@ -14,27 +15,9 @@
 
  // Declarations
 
-    var Blob, comm, secret, Store;
+    var comm, secret, Resource, Reference, Storage;
 
  // Definitions
-
-    Blob = function Blob(obj) {
-     // This function needs documentation.
-        var state, that;
-        state = {};
-        that = this;
-        Object.defineProperty(that, 'comm', {
-            configurable: false,
-            enumerable: false,
-            writable: false,
-            value: function (x) {
-             // This function needs documentation.
-                return comm.call(that, state, x);
-            }
-        });
-        // ...
-        return that;
-    };
 
     comm = function (inner, outer) {
      // This function needs documentation.
@@ -52,7 +35,43 @@
 
     secret = {};
 
-    Store = function Store(obj) {
+    Reference = function Reference(obj) {
+     // This function needs documentation.
+        var state, that;
+        state = {};
+        that = this;
+        Object.defineProperty(that, 'comm', {
+            configurable: false,
+            enumerable: false,
+            writable: false,
+            value: function (x) {
+             // This function needs documentation.
+                return comm.call(that, state, x);
+            }
+        });
+        // ...
+        return that;
+    };
+
+    Resource = function Resource(obj) {
+     // This function needs documentation.
+        var state, that;
+        state = {};
+        that = this;
+        Object.defineProperty(that, 'comm', {
+            configurable: false,
+            enumerable: false,
+            writable: false,
+            value: function (x) {
+             // This function needs documentation.
+                return comm.call(that, state, x);
+            }
+        });
+        // ...
+        return that;
+    };
+
+    Storage = function Storage(obj) {
      // This function needs documentation.
         var state, that;
         state = {};
@@ -63,14 +82,14 @@
 
  // Prototype definitions
 
-    Blob.prototype.sync = function (x) {
+    Resource.prototype.sync = function (x) {
      // This function needs documentation.
         var y;
         // ...
         return y;
     };
 
-    Store.prototype.query = function (x) {
+    Storage.prototype.query = function (x) {
      // This function needs documentation.
         var y;
         // ...
@@ -80,13 +99,18 @@
  // Out-of-scope definitions
 
     (function () {
+     // I have used an anonymous closure here mainly to "quarantine" all of the
+     // code specific to Node.js into a single area. This strategy allows me to
+     // minimize the number of exceptions I allow JSLint to make and thus helps
+     // keep the abstractions portable even when the rest of the project isn't.
         /*jslint node: true */
-        if (typeof exports === 'object') {
-            exports.database = function (obj) {
-             // This function needs documentation.
-                return new Store(obj);
-            };
+        if (typeof exports !== 'object') {
+            return;
         }
+        exports.database = function (obj) {
+         // This function needs documentation.
+            return new Storage(obj);
+        };
         return;
     }());
 
